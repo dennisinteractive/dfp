aa2f9743b2daf62c70b81b0f0bd9a6d174552956
Scott Alan Henry <shenry@phase2technology.com>
2017-12-29 15:04:44 -0600

    lazyLoad: This is unecessary now with the updates for lazy loading ads

--- theme/dfp_tag.tpl.php
+++ theme/dfp_tag.tpl.php
@@ -9,15 +9,4 @@
   <?php if (isset($slug)):
     print drupal_render($slug);
   endif; ?>
-  <script type="text/javascript">
-    <?php if (!empty($tag->disable_initial_load)): ?>
-    // MIA
-    <?php else: ?>
-    jQuery(document).ready(function() {
-      googletag.cmd.push(function() {
-        googletag.display("<?php print $tag->placeholder_id ?>");
-      });
-    });
-    <?php endif; ?>
-  </script>
 </div>
