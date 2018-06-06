0bb1f5ad00cfe8f0f3b5a71b06ccdede85fcc97a
scottalan <scottalan@378998.no-reply.drupal.org>
2018-06-06 16:37:28 -0500

    Issue #2977941 by scottalan: Ensure the id exists in the isInViewport function

--- js/jquery.googletag.js
+++ js/jquery.googletag.js
@@ -1,12 +1,15 @@
 (function ($) {
-  "use strict";
+    "use strict";
 
-  $.fn.isInViewport = function() {
-    var elementTop = $(this).offset().top;
-    var elementBottom = elementTop + $(this).outerHeight();
-    var viewportTop = $(window).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
-    return elementBottom > viewportTop && elementTop < viewportBottom;
-  };
+    $.fn.isInViewport = function() {
+        if ($(this).length === 0) {
+            return false;
+        }
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
 
 })(jQuery);
